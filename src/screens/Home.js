34578ca54f5252359306ec3a62c7d285790c1903
Eduardo F Zimmermann <edu.zimmermann.efz@gamil.com>
2022-11-09 10:11:46 -0300

    Criado dois components Button e estilizado div da Home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,9 +1,11 @@
+import { Background_Img_Home } from "../common-components/Background_Img_Home/Background_Img_Home";
 import { Header } from "../common-components/Header/Header";
 
 export function Home() {
     return (
         <div>
             <Header />
+            <Background_Img_Home />
         </div>
     );
 }
