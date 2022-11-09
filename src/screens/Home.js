0de653d51663f2c5878b673ff2bfb0f62644351a
Eduardo F Zimmermann <edu.zimmermann.efz@gamil.com>
2022-11-09 10:19:08 -0300

    Arrumado screen Home com suas respectivas divs no próprio arquivo

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,26 @@
-import { Background_Img_Home } from "../common-components/Background_Img_Home/Background_Img_Home";
 import { Header } from "../common-components/Header/Header";
+import { Button_Destinos } from "../common-components/Buttons/Buttons_Destinos";
+import styled from "styled-components";
+import img from "./../assets/images/background_home.png";
+
+const Background_img = styled.div`
+    width: 100%;
+    height: 110vh;
+    display: flex;
+    padding: 0 0 0 5vw;
+    background-image: url(${img});
+    background-repeat: no-repeat;
+    background-size: cover;
+    align-items: center;
+`;
 
 export function Home() {
     return (
         <div>
             <Header />
-            <Background_Img_Home />
+            <Background_img>
+                <Button_Destinos>Confira destinos inclusivos</Button_Destinos>
+            </Background_img>
         </div>
     );
 }
