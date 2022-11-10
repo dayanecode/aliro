959250d156fe5eb0e7307f9eec28353d6c9d6df0
dayanecode <dayanecode@gmail.com>
2022-11-09 23:12:35 -0300

    insere dados dos integrantes no footer e cria componente de texto h3

---
+++ src/common-components/Descriptions/Descriptions_h3.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import { Colors, FontFamilies, FontSizes, FontWeights } from "../../shared/DesignTokens";
+
+export const Description_h3 = styled.span`
+    font-family: ${FontFamilies.PRIMARY};
+    font-weight: ${FontWeights.ULTRA_BOLD};
+    font-size: ${FontSizes.TWO};
+    color: ${Colors.NEUTRAL_BLACK};
+`;
--- src/common-components/Footer/Footer.js
+++ src/common-components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { Colors } from "../../shared/DesignTokens";
-import { Description_h2 } from "../Descriptions/Descriptions_h2";
+import { Description_h3 } from '../Descriptions/Descriptions_h3';
 import { Description_p } from "../Descriptions/Descriptions_p";
 import img from "./../../assets/icons/logoStellantis.png"
 import img2 from "./../../assets/icons/logo_github.png"
@@ -48,7 +48,7 @@ const Image = styled.img.attrs({
 
 const GitHub = styled.img.attrs({
     src: img2,
-    alt: 'Logotipo Stelantis'
+    alt: 'Logotipo GitHub'
 }) `
     width: 2vw;
     margin: 0 1vw 0 0;
@@ -104,6 +104,14 @@ const Div2_4 = styled.div`
     justify-content: space-evenly;
 `;
 
+const Div2_5 = styled.div`
+    width: 10%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+`;
+
 const DivGit = styled.div`
     width: 50%;
     display: flex;
@@ -121,7 +129,7 @@ export function Footer(){
             <Wrapper>
                 <Div1>
                     <Div1_1>
-                        <Description_h2>FIAP | 2TINOR-2022</Description_h2>
+                        <Description_h3>FIAP | 2TINOR-2022</Description_h3>
                         <Description_p>SISTEMAS PARA INTERNET</Description_p>
                         <Description_p>GRUPO X</Description_p>
                     </Div1_1>
@@ -136,40 +144,50 @@ export function Footer(){
 
                 <Div2>
                     <Div2_1>
-                        <Description_h2>Nome completo</Description_h2>
-                        <Description_p>RA 00000</Description_p>
+                        <Description_h3>Dayane Ramos do Nascimento</Description_h3>
+                        <Description_p>RM 92632</Description_p>
                         <DivGit>
                         <GitHub />
-                            <Description_p>GitHub</Description_p>
+                            <Description_p>dayanecode</Description_p>
                         </DivGit>
                     </Div2_1>
 
                     <Div2_2>
-                    <Description_h2>Nome completo</Description_h2>
-                        <Description_p>RA 00000</Description_p>
+                    <Description_h3>Eduardo Feliciano Zimmermann</Description_h3>
+                        <Description_p>RM 92197</Description_p>
                         <DivGit>
                         <GitHub />
-                            <Description_p>GitHub</Description_p>
+                            <Description_p>DufZ</Description_p>
                         </DivGit>
                     </Div2_2>
 
                     <Div2_3>
-                    <Description_h2>Nome completo</Description_h2>
-                        <Description_p>RA 00000</Description_p>
+                    <Description_h3>Gabriel Dovnorovzki Rodrigues</Description_h3>
+                        <Description_p>RM 92399</Description_p>
                         <DivGit>
                         <GitHub />
-                            <Description_p>GitHub</Description_p>
+                            <Description_p>gabsdovz</Description_p>
                         </DivGit>
                     </Div2_3>
 
                     <Div2_4>
-                    <Description_h2>Nome completo</Description_h2>
-                        <Description_p>RA 00000</Description_p>
+                    <Description_h3>Henrique Aslan Marques Pina</Description_h3>
+                    <Description_p>RM 92542</Description_p>
                         <DivGit>
                         <GitHub />
-                            <Description_p>GitHub</Description_p>
+                            <Description_p>HMpina</Description_p>
                         </DivGit>
                     </Div2_4>
+
+                    <Div2_5>
+                    <Description_h3>Henrique Fernandez Soares</Description_h3>
+                        <Description_p>RM 92489</Description_p>
+                        <DivGit>
+                        <GitHub />
+                            <Description_p>RickFernandez</Description_p>
+                        </DivGit>
+                    </Div2_5>
+
                 </Div2>
             </Wrapper>
         </>
